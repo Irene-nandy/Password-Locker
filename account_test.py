634aa02805c99887eeac7b4892e87bbef0b2e565
Irene-nandy <irenekasiva75@gmail.com>
2020-09-10 10:48:39 +0300

    add delete and exist  account

--- account_test.py
+++ account_test.py
@@ -37,40 +37,40 @@ class TestAccount(unittest.TestCase):
         test_account.save_account()
         self.assertEqual(len(Account.account_list),2)
 
-#     def test_delete_account(self):
-#         """
-#         test_delete_account to test if we can remove an account from our account list
-#         """
-#         self.new_account.save_account()
-#         test_account = Account("irene","facebook","irene254","irene@facebook.com")
-#         test_account.save_account()
+    def test_delete_account(self):
+        """
+        test_delete_account to test if we can remove an account from our account list
+        """
+        self.new_account.save_account()
+        test_account = Account("irene","facebook","irene254","irene@facebook.com")
+        test_account.save_account()
 
-#         self.new_account.delete_account()# deleting the account object
-#         self.assertEqual(len(Account.account_list),1)
+        self.new_account.delete_account()# deleting the account object
+        self.assertEqual(len(Account.account_list),1)
 
-#     def test_find_account_by_account_name(self):
-#         """
-#         test to check if we can find an account by account_name and display information
-#         """  
-#         self.new_account.save_account()
-#         test_account = Account("irene","facebook","irene254","irene@facebook.com")
-#         test_account.save_account()  
+    def test_find_account_by_account_name(self):
+        """
+        test to check if we can find an account by account_name and display information
+        """  
+        self.new_account.save_account()
+        test_account = Account("irene","facebook","irene254","irene@facebook.com")
+        test_account.save_account()  
 
-#         found_account = Account.find_by_name("facebook")
-#         self.assertEqual(found_account.email,test_account.email)
+        found_account = Account.find_by_name("facebook")
+        self.assertEqual(found_account.email,test_account.email)
 
-#     def test_account_exists(self):
-#         '''
-#         test to check if we can return a Boolean  if we cannot find the account.
-#         '''
+    def test_account_exists(self):
+        '''
+        test to check if we can return a Boolean  if we cannot find the account.
+        '''
 
-#         self.new_account.save_account()
-#         test_account = Account("irene","facebook","irene254","irene254@facebook.com") # Creating new account
-#         test_account.save_account()
+        self.new_account.save_account()
+        test_account = Account("irene","facebook","irene254","irene254@facebook.com") # Creating new account
+        test_account.save_account()
 
-#         account_exists = Account.account_exist("irene254")
+        account_exists = Account.account_exist("irene254")
 
-#         self.assertTrue(account_exists)
+        self.assertTrue(account_exists)
 
 #     def test_display_all_accounts(self):
 #         '''
