da1bb4a38945f34d2a2f06c3622f91cec5e1d0b6
Irene-nandy <irenekasiva75@gmail.com>
2020-09-10 10:51:04 +0300

    add changes to the credentials

--- credentials.py
+++ credentials.py
@@ -16,3 +16,32 @@ class Credentials:
         Method to save credential objects into credentials list
         """  
         Credentials.credentials_list.append(self)
+    
+        def delete_credentials(self):
+        """
+        A method that deletes credentials from the list
+        """
+        Credentials.credentials_list.remove(self)
+
+    @classmethod
+    def find_by_name(cls,name):
+         for credentials in cls.credentials_list:
+            if credentials.credentials_name == name:
+             return credentials
+
+    @classmethod
+    def credentials_exist(cls,name):
+        """
+        """
+        for credentials in cls.credentials_list:
+            if credentials.password == name:
+                    return credentials
+
+        return False
+
+    @classmethod
+    def display_credentials(cls): 
+        '''
+        method that returns the credentials list
+        '''
+        return cls.credentials_list
